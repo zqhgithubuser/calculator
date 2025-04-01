2319350454e826a30be623575803b52128eac87e
zqh1209 <3535521945@qq.com>
2025-04-01 17:43:12 +0800

    Add sum logic, controller and unit test

---
+++ src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.example.calculator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
